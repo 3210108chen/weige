9212c036288ec122218816b69b1dd67844373e1a
weige <284556010@qq.com>
2015-07-29 13:46:38 +0800

    初始化weige

---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by rimi on 15/7/29.
+//  Copyright (c) 2015年 rimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
